d20a5533742156dcae64b3607b47f03f23dfb10f
Darwin Raudales <147955591+DRWcode@users.noreply.github.com>
2025-02-11 16:41:14 -0600

    Update server.js
    se agrego plantilla para codiog

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// Endpoint para productos
+
+// Endpoint para usuarios
+
+
+// Endpoint para categorías
+
+
+// Endpoint para pedidos
+
+
+app.listen(PORT, ()=>{
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
