044864899e97f9bc93d220fb9739e11f261195f8
MoiPad <moipad85@gmail.com>
2025-02-11 19:31:44 -0600

    Agregue la API de usuarios

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 3000;
 
 // Endpoint para productos
 
 // Endpoint para usuarios
+const usuariosRouter = require("./usuarios.js");
 
+app.use("/usuarios", usuariosRouter);
 
 // Endpoint para categorías
 
-
 // Endpoint para pedidos
 
-
-app.listen(PORT, ()=>{
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
